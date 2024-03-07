87b7eb288acfaf856f8634d747ec1581da116847
Simon-157 <boatengsimonjnr157@gmail.com>
2024-03-07 00:51:58 +0000

    deploy changes for env

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const { Pool } = require("pg");
-
+require("dotenv").config();   
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const pool = new Pool({
   user: "simon",
